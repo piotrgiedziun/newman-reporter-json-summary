c42fc855bcf2bc9a3445d3be8c03e6ff0bede7a0
Piotr Giedziun <piotr.giedziun@identt.pl>
2022-02-25 13:00:04 +0100

    bump

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function requestUrlToString(r) {
-    return `${r.protocol}://${r.host.join(".")}/${r.path.join("/")}`;
-}
-
 function createSummary(summary) {
     return summary.run.executions.map((e) => {
         return JSON.stringify({
             "type": "newman_test",
             "collection_name": summary.collection.name,
             "name": e.item?.name,
-            "url": requestUrlToString(e.item.request.url),
+            "url": e.item?.request?.url?.path?.join("/"),
             "method": e.item?.request?.method,
             "responseTime": e.response?.responseTime,
             "responseCode": e.response?.code,
@@ -18,13 +14,14 @@ function createSummary(summary) {
 }
 
 module.exports = function(newman, options) {
+    console.log(options)
     newman.on('beforeDone', function(err, data) {
         if (err) { return; }
 
         newman.exports.push({
             name: '@npm_identt/newman-reporter-log',
             default: 'summary.log',
-            path:  options.summaryJsonExport,
+            path:  options.logExport,
             content: createSummary(data.summary).join("\n")
         });
     });
